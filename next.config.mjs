e661f344bc789fb35296be5f8adc48ca5078b4e4
saadansari234 <saadansari3212@gmail.com>
2024-04-23 23:10:21 +0530

    config return

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,7 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     output:"export",
-// };
-
-// export default nextConfig;
-
-// next.config.js
-
 const nextConfig = {
-    target: 'experimental-serverless-trace',
-    exportPathMap: async function () {
-        return {
-            '/': { page: '/' },
-            '/about': { page: '/about' },
-            // Add other routes here
-        };
-    },
+    output:"export",
 };
 
 export default nextConfig;
+
